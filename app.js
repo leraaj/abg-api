c95427399449baf3eef6011f463a6ae0d0e603bd
leraaj <leraalfredojohn@gmail.com>
2025-06-29 07:19:43 +0800

    test 5

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: true, // true if using HTTPS
+      secure: false, // true if using HTTPS
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24, // 1 day
     },
